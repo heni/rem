4daef86029033001efda1618a0344176be91ab2c
heni <Eugene.Krokhalev@gmail.com>
2015-11-28 23:13:04 +0000

    increase version and support pip

--- client/setup.py
+++ client/setup.py
@@ -20,20 +20,21 @@ setup(
     description = "client library for REM server; see at https://github.com/heni/rem",
     maintainer = "Eugene Krokhalev",
     maintainer_email = "Eugene.Krokhalev@gmail.com",
-    version = "1.0.1",
+    version = "1.0.2",
     packages = [ "remclient" ],
     package_dir = { '': "src" },
     scripts = [ "rem-tool" ],
     install_requires = [ "six", "bsddb3" ],
     classifiers = [
-        "Development Status :: 5 - Production",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "Operating System :: Linux",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: System :: Systems Administration",
-        "Topic :: Software Development :: Automation"
+        "Topic :: Software Development"
     ]
 )
