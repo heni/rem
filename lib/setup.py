82cd77530bc82752515bfbf373dd99cdfbcb71f0
Eugene Krokhalev <ekrokhalev@c.snap.com>
2020-08-03 18:37:06 +0300

    update setup scripts for setuptools support

--- lib/setup.py
+++ lib/setup.py
@@ -1,20 +1,7 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import os
-try:
-    from ConfigParser import ConfigParser
-except ImportError:
-    from configparser import ConfigParser
-
-
-def LoadMetadata():
-    setup_dir = os.path.dirname(__file__)
-    cp = ConfigParser()
-    cp.read(os.path.join(setup_dir, "setup.cfg"))
-    return dict(cp.items("global"))
 
 
 setup(
-    name="_fork_locking",
     ext_modules=[Extension("_fork_locking", ["_fork_locking.c"])],
-    **LoadMetadata()
 )
