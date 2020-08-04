9f505fb2de297c44fb4e13c2b50afbbd48e886c0
Eugene Krokhalev <ekrokhalev@c.snap.com>
2020-08-04 16:03:22 +0300

    update libraries for use of fresh setuptools

--- client/setup.py
+++ client/setup.py
@@ -3,7 +3,7 @@ import os
 
 def prepare_sources():
     srcdir = "src/remclient"
-    modules = ["__init__.py", "remclient.py", "constants.py"] 
+    modules = ["__init__.py", "remclient.py", "constants.py"]
     if not os.path.isdir(srcdir):
         os.makedirs(srcdir)
     for filename in modules:
@@ -16,25 +16,7 @@ def prepare_sources():
 prepare_sources()
 
 setup(
-    name = "remclient",
-    description = "client library for REM server; see at https://github.com/heni/rem",
-    maintainer = "Eugene Krokhalev",
-    maintainer_email = "Eugene.Krokhalev@gmail.com",
-    version = "1.0.6",
     packages = [ "remclient" ],
     package_dir = { '': "src" },
     scripts = [ "rem-tool" ],
-    install_requires = [ "six"],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Operating System :: POSIX :: BSD",
-        "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Topic :: System :: Systems Administration",
-        "Topic :: Software Development"
-    ]
 )
---
+++ lib/README.md
@@ -0,0 +1,2 @@
+### native-fork-locking Library
+This module provides locks that can be safely used for frequently forked applications
